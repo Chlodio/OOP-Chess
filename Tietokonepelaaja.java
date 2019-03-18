b9e4ccace07ebfff9e079a92657b65fe4ea1bf01
Aoruno <47475600+Aoruno@users.noreply.github.com>
2019-03-18 14:14:52 +0200

    Update Tietokonepelaaja.java

--- Tietokonepelaaja.java
+++ Tietokonepelaaja.java
@@ -25,11 +25,11 @@ class Tietokonepelaaja extends Pelaaja {
 		for(int i = 0; i < 7; i++){
 			for(int j = 0; i < 7; j++){
 				if (omanVarinNappulat[i][j] > 0){
-					int[][] siirrot = pelilauta.annaLauta[i][j].siirrot();
+					int[][] siirrot = pelilauta.annaNappula(i, j).siirrot();
 					int[][] sallitutSiirrot = pelilauta.testaaSiirrot(siirrot, pelilauta.annaNappula(i, j));
 					for (int k = 0; k < 7; k++){
 						for (int l = 0; l < 7; l++){
-							if (sallitutSiirrot[k][l] > 0 && vastustajanNappulat[k][l] > 0 && pelilauta.annaLauta[i][j].annaArvo() < parhaanNappulanArvo && !pelilauta.itsemurha(pelilauta.annaNappula(i, j)), k, l){
+							if (sallitutSiirrot[k][l] > 0 && vastustajanNappulat[k][l] > 0 && pelilauta.annaNappula(i, j).annaArvo() < parhaanNappulanArvo && !pelilauta.itsemurha(pelilauta.annaNappula(i, j)), k, l){
 								int parhaanNappulanLahtopaikkaNo = i;
 								int parhaanNappulanLahtopaikkaAbc = j;
 								int parhaanNappulanSijoituspaikkaNo = k;
@@ -51,7 +51,7 @@ class Tietokonepelaaja extends Pelaaja {
 			}
 		}
 		if (parhaanNappulanArvo < 10){
-			pelilauta.liiku(pelilauta.annaLauta()[parhaanNappulanLahtopaikkaNo][parhaanNappulanLahtopaikkaAbc], parhaanNappulanSijoituspaikkaNo, parhaanNappulanSijoituspaikkaAbc);
+			pelilauta.liiku(pelilauta.annaNappula(parhaanNappulanLahtopaikkaNo, parhaanNappulanLahtopaikkaAbc), parhaanNappulanSijoituspaikkaNo, parhaanNappulanSijoituspaikkaAbc);
 		}
 		else {
 			while (1 == 1){
