23cf854ff07bf2169a6b2269014fa6cdcd6bb6d8
Chlodio <21118398+Chlodio@users.noreply.github.com>
2019-03-18 18:06:22 +0200

    Update Ihmispelaaja.java

--- Ihmispelaaja.java
+++ Ihmispelaaja.java
@@ -16,8 +16,10 @@ class Ihmispelaaja extends Pelaaja {
 		Nappula nappula = /*pelaajan valitsema nappula omanVarinNappuloista*/;
 		int[][] siirrot = nappula.siirrot(); /*JUHA: parametriksi tarvitaan Pelilauta*/
 		int[][] sallitutSiirrot = pelilauta.testaaSiirrot(siirrot, nappula);
-		int uusiSijaintiNo =/*pelaajan valitsema uusi sijainti sallitutSiirroista*/;
-		int uusiSijaintiAbc = /*pelaajan valitsema uusi sijainti sallitutSiirroista*/;
+		Pelilauta.varitaRuudut(sallitutSiirrot);
+		int uusiSijaintiNo =	x%8  	/*pelaajan valitsema uusi sijainti sallitutSiirroista*/;
+		int uusiSijaintiAbc = 	x/8	/*pelaajan valitsema uusi sijainti sallitutSiirroista*/;
+		Pelilauta.normaalisoiRuudut(sallitutSiirrot);
 		/*JUHA: tähän voi lisätä vielä itse-murhatestin. Eli kyseinen siirto voidaan testata metodilla pelilauta.itsemurha(Nappula,int,int). Jos true, niin sitten pitäisi kysyä uutta siirtoa*/
 		/*JUHA: huom: sallitutSiirrot ei palauta linnoitusmahdollisuutta. Tämä pitää kysyä erikseen pelilauta.linnoitusMahdollista(Kuningas,Torni)*/
 		pelilauta.liiku(nappula, uusiSijaintiNo, uusiSijaintiAbc);
