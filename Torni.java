925c6fec4bd2cf3af4dc352da4464f8bf357ee08
juvirta <47291692+juvirta@users.noreply.github.com>
2019-03-18 13:23:49 +0200

    Create Torni.java

---
+++ Torni.java
@@ -0,0 +1,69 @@
+public class Torni extends Nappula{
+    
+  public Torni(boolean vari){
+    this.vari = vari;
+    this.arvo = 5;
+    this.liikkunut = false;
+    this.elossa = true;
+    if (!vari){ 
+       this.symbooli = "♖";
+    } else {
+      this.symbooli = "♜";
+    }
+  }
+  
+  public boolean annaVari(){
+    return this.vari;
+  }
+  
+  public void asetaElossa(boolean elossa){
+    this.elossa = elossa;
+  }
+  
+  public boolean annaElossa(){
+    return this.elossa;
+  }
+  
+  public int annaArvo(){
+    return this.arvo;
+  }
+  
+  public void asetaLiikkunut(){
+    this.liikkunut = true;
+  }
+  
+  public boolean annaLiikkunut(){
+    return this.liikkunut;
+  }
+  
+  public int[][] siirrot(Pelilauta lauta){
+    int sNo = lauta.annaSijaintiNo(this);
+    int sAbc = lauta.annaSijaintiAbc(this);
+    int[][] siirrot = new int[8][8];
+    int eka = sNo;
+    while (eka < 7){
+      siirrot[eka+1][sAbc]=1;
+      eka++;
+    }
+    eka = sNo;
+    while (eka > 0){
+      siirrot[eka-1][sAbc]=1;
+      eka--;
+    }
+    eka = sAbc;
+    while (eka > 0 ){
+      siirrot[sNo][eka-1]=1;
+      eka--;
+    }
+    eka = sAbc;
+    while (eka < 7 ){
+      siirrot[sNo][eka+1]=1;
+      eka++;
+    }
+    return siirrot;
+  }
+  
+   public void muutu(Pelilauta lauta){
+  }
+   
+}
