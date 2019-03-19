9bc3e453813ede496a5e48075c8bab1c71e3435b
Chlodio <chlodio@hotmail.com>
2019-03-19 11:07:31 +0200

    Lauta -> Pelilauta

--- Main.java
+++ Main.java
@@ -15,7 +15,7 @@ class Main{
 	public static void main(String[] args){
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run(){
-				Lauta.createGUI();
+				Pelilauta.createGUI();
 			}
 		});
 	}
