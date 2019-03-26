cd74b0c2a3909b8a6a0a085d7c33af9b4119bec0
Aoruno <47475600+Aoruno@users.noreply.github.com>
2019-03-26 11:27:11 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 
 class Main{
-	private static Peli TPeli;
+ private static Peli TPeli;
  public static void main(String[] args){
   SwingUtilities.invokeLater(new Runnable() {
    public void run(){
@@ -21,33 +21,14 @@ class Main{
   });
   Peli peli = new Peli();
   TPeli = peli;
-  boolean tallennettuPeli = false;
-  try {
-    tallennettuPeli = peli.tallennettuToimivaPeli();
-  }
-  catch (IOException e) {
-    System.out.println("Tallennettua tiedostoa ei ole");
-  }
-  if (tallennettuPeli){
-    if (1 == 1){  /*valinta on tallennettu peli*/
-      try{
-        peli.lataaPeliAsetuksilla();
-      }
-      catch (IOException e){
-        System.out.println("Ei pitäisi päästä tänne asti");
-	}
-    }
-  }
-  else {
-   peli.asetaPeliAsetuksilla();
-  }
+  peli.asetaPeliAsetuksilla();
   peli.pelaa();
 
-  //java.lang.System.exit(0);		//tarvitaanko me sulkeutumista, jos GUI hallitsee sitä? //Ei tarvita
+  //java.lang.System.exit(0);  //tarvitaanko me sulkeutumista, jos GUI hallitsee sitÃ¤?
  }
 
  public static Peli getTPeli(){
-	 return TPeli;
+  return TPeli;
  }
 
 }
